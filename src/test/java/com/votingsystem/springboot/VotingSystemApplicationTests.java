889f4a4825938a78f5dc1480dbed28ec010c82b3
Иван <vania.muratoff@yandex.ru>
2021-09-09 16:05:32 +0700

    init project

---
+++ src/test/java/com/votingsystem/springboot/VotingSystemApplicationTests.java
@@ -0,0 +1,11 @@
+package com.votingsystem.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSystemApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
