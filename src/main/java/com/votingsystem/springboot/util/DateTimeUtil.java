3ea741f75630a5909dddccda5d286f6b48c85638
Иван <vania.muratoff@yandex.ru>
2021-09-11 15:54:25 +0700

    add Clock

--- src/main/java/com/votingsystem/springboot/util/DateTimeUtil.java
+++ src/main/java/com/votingsystem/springboot/util/DateTimeUtil.java
@@ -2,13 +2,37 @@ package com.votingsystem.springboot.util;
 
 import lombok.experimental.UtilityClass;
 
+import java.time.Clock;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.ZoneId;
 
 @UtilityClass
 public class DateTimeUtil {
     public static final LocalTime BORDER_TIME_FOR_CANCEL_OF_VOTING = LocalTime.of(10, 59, 59);
 
+    private static final ZoneId zoneId = ZoneId.systemDefault();
+
+    private static Clock clock = Clock.systemDefaultZone();
+
+    public static LocalTime now() {
+        return LocalTime.now(getClock());
+    }
+
+    //https://stackoverflow.com/a/29360514
+    public static void useFixedClockAt(LocalDateTime date) {
+        clock = Clock.fixed(date.atZone(zoneId).toInstant(), zoneId);
+    }
+
+    public static void useSystemDefaultZoneClock() {
+        clock = Clock.systemDefaultZone();
+    }
+
+    private static Clock getClock() {
+        return clock;
+    }
+
     public static boolean isTimeToCancelVote() {
-        return LocalTime.now().compareTo(BORDER_TIME_FOR_CANCEL_OF_VOTING) < 0;
+        return now().compareTo(BORDER_TIME_FOR_CANCEL_OF_VOTING) < 0;
     }
 }
--- src/test/java/com/votingsystem/springboot/web/RestaurantControllerTest.java
+++ src/test/java/com/votingsystem/springboot/web/RestaurantControllerTest.java
@@ -6,9 +6,9 @@ import com.votingsystem.springboot.model.Restaurant;
 import com.votingsystem.springboot.model.Vote;
 import com.votingsystem.springboot.repository.RestaurantRepository;
 import com.votingsystem.springboot.repository.VoteRepository;
+import com.votingsystem.springboot.util.DateTimeUtil;
 import com.votingsystem.springboot.util.JsonUtil;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
@@ -16,14 +16,19 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.util.NestedServletException;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static com.votingsystem.springboot.RestaurantTestData.*;
 import static com.votingsystem.springboot.UserTestData.votedUser;
-import static com.votingsystem.springboot.util.DateTimeUtil.isTimeToCancelVote;
+import static com.votingsystem.springboot.util.DateTimeUtil.BORDER_TIME_FOR_CANCEL_OF_VOTING;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class RestaurantControllerTest extends AbstractControllerTest {
     static final String URL = RestaurantController.URL + '/';
 
@@ -36,11 +41,11 @@ public class RestaurantControllerTest extends AbstractControllerTest {
     @Test
     @WithUserDetails(value = UserTestData.ADMIN_MAIL)
     void get() throws Exception {
-        perform(MockMvcRequestBuilders.get(URL + RestaurantTestData.RESTAURANT_ID1))
+        perform(MockMvcRequestBuilders.get(URL + RESTAURANT_ID1))
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(RestaurantTestData.MATCHER.contentJson(RestaurantTestData.restaurant1));
+                .andExpect(MATCHER.contentJson(restaurant1));
     }
 
     @Test
@@ -50,16 +55,16 @@ public class RestaurantControllerTest extends AbstractControllerTest {
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(RestaurantTestData.MATCHER.contentJson(RestaurantTestData.restaurant1, RestaurantTestData.restaurant2, RestaurantTestData.restaurant3));
+                .andExpect(MATCHER.contentJson(restaurant1, restaurant2, restaurant3));
     }
 
     @Test
     @WithUserDetails(value = UserTestData.ADMIN_MAIL)
     void delete() throws Exception {
-        perform(MockMvcRequestBuilders.delete(URL + RestaurantTestData.RESTAURANT_ID1))
+        perform(MockMvcRequestBuilders.delete(URL + RESTAURANT_ID1))
                 .andDo(print())
                 .andExpect(status().isOk());
-        Assertions.assertFalse(repository.findById(RestaurantTestData.RESTAURANT_ID1).isPresent());
+        Assertions.assertFalse(repository.findById(RESTAURANT_ID1).isPresent());
     }
 
     @Test
@@ -71,60 +76,68 @@ public class RestaurantControllerTest extends AbstractControllerTest {
                 .content(JsonUtil.writeValue(newRestaurant)))
                 .andDo(print())
                 .andExpect(status().isCreated());
-        Restaurant created = RestaurantTestData.MATCHER.readFromJson(action);
+        Restaurant created = MATCHER.readFromJson(action);
         int newId = created.id();
         newRestaurant.setId(newId);
-        RestaurantTestData.MATCHER.assertMatch(created, newRestaurant);
-        RestaurantTestData.MATCHER.assertMatch(repository.getById(newId), newRestaurant);
+        MATCHER.assertMatch(created, newRestaurant);
+        MATCHER.assertMatch(repository.getById(newId), newRestaurant);
     }
 
     @Test
     @WithUserDetails(value = UserTestData.ADMIN_MAIL)
     void update() throws Exception {
         Restaurant updated = RestaurantTestData.getUpdated();
-        perform(MockMvcRequestBuilders.put(URL + RestaurantTestData.RESTAURANT_ID1)
+        perform(MockMvcRequestBuilders.put(URL + RESTAURANT_ID1)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(JsonUtil.writeValue(updated)))
                 .andDo(print())
                 .andExpect(status().isOk());
-        RestaurantTestData.MATCHER.assertMatch(updated, repository.findById(RestaurantTestData.RESTAURANT_ID1).orElseThrow());
+        MATCHER.assertMatch(updated, repository.findById(RESTAURANT_ID1).orElseThrow());
     }
 
     @Test
     @WithUserDetails(value = UserTestData.ADMIN_MAIL)
     void getWithMenu() throws Exception {
-        perform(MockMvcRequestBuilders.get(URL + RestaurantTestData.RESTAURANT_ID1 + "/with-menu"))
+        perform(MockMvcRequestBuilders.get(URL + RESTAURANT_ID1 + "/with-menu"))
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(RestaurantTestData.WITH_MENU_MATCHER.contentJson(RestaurantTestData.restaurant1));
+                .andExpect(WITH_MENU_MATCHER.contentJson(restaurant1));
     }
 
+    @Order(1)
     @Test
     @WithUserDetails(value = UserTestData.USER_MAIL)
     void doVote() throws Exception {
-        Restaurant voted = RestaurantTestData.getVoted();
+        Restaurant voted = getVoted();
         perform(MockMvcRequestBuilders.patch(URL + RestaurantTestData.RESTAURANT_ID1)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(JsonUtil.writeValue(voted)))
                 .andExpect(status().isOk())
                 .andDo(print());
-        RestaurantTestData.WITH_VOTES_MATCHER.assertMatch(voted, repository.findById(RestaurantTestData.RESTAURANT_ID1).orElseThrow());
+        WITH_VOTES_MATCHER.assertMatch(voted, repository.findById(RESTAURANT_ID1).orElseThrow());
     }
 
     @Test
     @WithUserDetails(value = UserTestData.VOTED_USER_MAIL)
-    void cancelVote() throws Exception {
+    void forbiddenCancelVote() throws Exception {
         voteRepo.save(new Vote(votedUser, RestaurantTestData.restaurant3));
-        if (isTimeToCancelVote()) {
-            doCancelVote();
-        } else {
-            Throwable thrown = assertThrows(NestedServletException.class, this::doCancelVote);
-            assertNotNull(thrown.getMessage());
-        }
+        DateTimeUtil.useFixedClockAt(LocalDateTime.of(LocalDate.now(), BORDER_TIME_FOR_CANCEL_OF_VOTING.plusSeconds(1)));
+        Throwable thrown = assertThrows(NestedServletException.class, this::cancelVote);
+        assertNotNull(thrown.getMessage());
+        DateTimeUtil.useSystemDefaultZoneClock();
     }
 
-    void doCancelVote() throws Exception {
+    @Test
+    @WithUserDetails(value = UserTestData.VOTED_USER_MAIL)
+    void allowedCancelVote() throws Exception {
+        voteRepo.save(new Vote(votedUser, RestaurantTestData.restaurant3));
+        DateTimeUtil.useFixedClockAt(LocalDateTime.of(LocalDate.now(), BORDER_TIME_FOR_CANCEL_OF_VOTING.minusSeconds(10)));
+        cancelVote();
+        DateTimeUtil.useSystemDefaultZoneClock();
+    }
+
+    void cancelVote() throws Exception {
         perform(MockMvcRequestBuilders.patch(URL + "cancel-vote"))
                 .andDo(print())
                 .andExpect(status().isOk());
